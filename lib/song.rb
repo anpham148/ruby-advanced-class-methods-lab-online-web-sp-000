bbcf590478a79bb76c1f3d9e2d4d8b2f83d118d1
An Pham <anpham148@gmail.com>
2020-07-03 09:46:39 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -33,8 +33,7 @@ class Song
     self.all.find{|song| song.name == name}
   end
 
-  def self.find_or_create_by_name(name1)
-
+  def self.find_or_create_by_name(name)
     self.all.each do |song|
       binding.pry
       if song.name == name1
